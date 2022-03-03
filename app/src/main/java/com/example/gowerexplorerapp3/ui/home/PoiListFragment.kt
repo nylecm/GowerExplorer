6d454c11cf01557dc3bdc8ebb7b34889a4c21eb3
Michal Nylec <michalnylec@gmail.com>
2022-03-03 01:21:25 +0000

    home renamed to poi list

--- app/src/main/java/com/example/gowerexplorerapp3/ui/home/PoiListFragment.kt
+++ app/src/main/java/com/example/gowerexplorerapp3/ui/home/PoiListFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.gowerexplorerapp3.databinding.FragmentHomeBinding
+import com.example.gowerexplorerapp3.databinding.FragmentPoiListBinding
 
-class HomeFragment : Fragment() {
+class PoiListFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentPoiListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentPoiListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
