e5e36425dbded976804cfe416f1ba8ec3cbef93e
Michal Nylec <michalnylec@gmail.com>
2022-04-03 15:47:54 +0100

    User model updated better firebase code and also idAdmin is working

--- app/src/main/java/com/example/gowerexplorerapp3/model/UserModel.kt
+++ app/src/main/java/com/example/gowerexplorerapp3/model/UserModel.kt
@@ -1,18 +1,16 @@
 package com.example.gowerexplorerapp3.model
 
-import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class UserModel {
     //lateinit var email: String
+    var isAdmin: Boolean = false
     var points: Int = 0
     var poisExplored: Vector<String> = Vector()
-    //private val db = Firebase.firestore
+    private val db = Firebase.firestore
 
     /**
      * User model constructor that can construct an arbitrary user based of set params.
@@ -22,15 +20,16 @@ class UserModel {
     }
 
     /**
-     * User model constructor that can constrct a
+     * User model constructor that can constrct a...
      */
     constructor(mAuth: FirebaseAuth) {
         //email = mAuth.currentUser?.email.toString()
-        val uid = mAuth.currentUser?.uid
+        addNewUserToDB(mAuth.currentUser!!.uid)
+    }
 
-        var db = FirebaseFirestore.getInstance()
+    private fun addNewUserToDB(uid: String) {
         db.collection("users")
-            .document(uid!!)
+            .document(uid)
             .set(this)
     }
 }
