fe367582894821c1f42646408bcf22edc2bb2599
Michal Nylec <michalnylec@gmail.com>
2022-04-06 18:26:25 +0100

    login and register activity added

---
+++ app/src/main/java/com/example/gowerexplorerapp3/ui/logreg/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gowerexplorerapp3.ui.logreg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gowerexplorerapp3.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
